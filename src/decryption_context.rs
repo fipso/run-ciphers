f60146174b9a13b6285b743d095b52757ac04a2f
Lee Bousfield <ljbousfield@gmail.com>
2016-08-09 22:12:02 -0400

    Added ciphertext hash to HTTP API

--- src/decryption_context.rs
+++ src/decryption_context.rs
@@ -5,11 +5,14 @@ use std::path::Path;
 use std::vec::Vec;
 use std::thread;
 
+extern crate md5;
+
 use utils;
 use openssl;
 
 #[derive(RustcEncodable)]
 pub struct CipherResult {
+  pub ciphertext: String,
   pub cipher: String,
   pub string: String
 }
@@ -18,6 +21,11 @@ pub struct DecryptionContext {
   ciphertexts: Vec<Vec<u8>>,
   ciphers: Vec<String>,
   salt: Vec<u8>
+}
+
+struct CiphertextThread {
+  ciphertext: String,
+  join_handle: thread::JoinHandle<Option<String>>
 }
 
 impl DecryptionContext {
@@ -78,26 +86,36 @@ impl DecryptionContext {
               unsafe {
                 let cipher_box = Box::from_raw(cipher);
                 let ciphertext = ciphertext.clone();
-                threads.push(thread::spawn(move || {
-                  match openssl::decrypt(&ciphertext, Box::into_raw(cipher_box), &key_iv_pair) {
-                    None => None,
-                    Some(data) => {
-                      match String::from_utf8(data) {
-                        Result::Ok(string) => Some(string),
-                        Result::Err(_) => None
+                threads.push(CiphertextThread {
+                  ciphertext: md5::compute(&ciphertext[..]).iter()
+                    .map(|n| format!("{:x}", n))
+                    .collect::<Vec<_>>()
+                    .concat(),
+                  join_handle: thread::spawn(move || {
+                    match openssl::decrypt(&ciphertext, Box::into_raw(cipher_box), &key_iv_pair) {
+                      None => None,
+                      Some(data) => {
+                        match String::from_utf8(data) {
+                          Result::Ok(string) => Some(string),
+                          Result::Err(_) => None
+                        }
                       }
                     }
-                  }
-                }));
+                  })
+                });
               }
             }
           }
           None => {}
         }
-        for thread in threads {
-          match thread.join() {
+        for ciphertext_thread in threads {
+          match ciphertext_thread.join_handle.join() {
             Ok(res) => match res {
-              Some(string) => result.push(CipherResult { cipher: cipher_name.clone(), string: string }),
+              Some(string) => result.push(CipherResult {
+                ciphertext: ciphertext_thread.ciphertext,
+                cipher: cipher_name.clone(),
+                string: string
+              }),
               None => {}
             },
             Err(_) => {}
